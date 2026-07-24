60eb7fb2871732091ba484970a03fb2694bee26e
Aaron Tushabe <tushabe.aaron@gmail.com>
2026-07-24 08:23:15 -0500

    Make Metadata.File default language configurable (#1)

--- io.openems.backend.metadata.file/test/io/openems/backend/metadata/file/MetadataFileTest.java
+++ io.openems.backend.metadata.file/test/io/openems/backend/metadata/file/MetadataFileTest.java
@@ -3,7 +3,9 @@ package io.openems.backend.metadata.file;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
@@ -11,6 +13,7 @@ import java.util.Map;
 import org.junit.Test;
 
 import io.openems.backend.common.test.DummyEventAdmin;
+import io.openems.common.session.Language;
 
 public class MetadataFileTest {
 
@@ -44,6 +47,69 @@ public class MetadataFileTest {
 				"apikey1", "edge1"), //
 				notification.getApikeysToEdgeIds());
 		assertFalse(notification.getApikeysToEdgeIds().containsKey(""));
+	}
+
+	@Test
+	public void testConfigurableDefaultLanguageAndInstanceIsolation() throws Exception {
+		final var path = createEmptyMetadataFile();
+		final var englishMetadata = createMetadataFile(path);
+		final var frenchMetadata = createMetadataFile(path);
+
+		activate(englishMetadata, path, Language.EN);
+		activate(frenchMetadata, path, Language.FR);
+
+		assertEquals(Language.EN, englishMetadata.getUser("admin").orElseThrow().getLanguage());
+		assertEquals(Language.FR, frenchMetadata.getUser("admin").orElseThrow().getLanguage());
+	}
+
+	@Test
+	public void testUpdateUserLanguageUpdatesSharedUser() throws Exception {
+		final var path = createEmptyMetadataFile();
+		final var sut = createMetadataFile(path);
+		activate(sut, path, Language.EN);
+
+		final var user = sut.getUser("admin").orElseThrow();
+		sut.updateUserLanguage(user, Language.NL);
+
+		assertEquals(Language.NL, sut.getUser("admin").orElseThrow().getLanguage());
+	}
+
+	private static Path createEmptyMetadataFile() throws Exception {
+		final var path = Files.createTempFile("metadata-file-language-test", ".json");
+		Files.writeString(path, """
+				{
+				  "edges": {}
+				}
+				""");
+		return path;
+	}
+
+	private static void activate(MetadataFile sut, Path path, Language defaultLanguage) throws Exception {
+		final Config config = new Config() {
+
+			@Override
+			public Class<? extends Annotation> annotationType() {
+				return Config.class;
+			}
+
+			@Override
+			public String path() {
+				return path.toString();
+			}
+
+			@Override
+			public Language defaultLanguage() {
+				return defaultLanguage;
+			}
+
+			@Override
+			public String webconsole_configurationFactory_nameHint() {
+				return "Metadata.File";
+			}
+		};
+		final Method activateMethod = MetadataFile.class.getDeclaredMethod("activate", Config.class);
+		activateMethod.setAccessible(true);
+		activateMethod.invoke(sut, config);
 	}
 
 	private static MetadataFile createMetadataFile(Path path) throws Exception {
